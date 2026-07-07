5aac1b6965dfff1332f22d00323ee9b938725213
Lorenzo Landi <l.landi@gdclouders.tech>
2026-07-07 14:36:31 +0200

    Fix output redirection in kube1-34-1.sh

--- Fedora/ProxMox/kube1-34-1.sh
+++ Fedora/ProxMox/kube1-34-1.sh
@@ -61,7 +61,7 @@ EOF
 
 
 
-cat <<EOF > /etc/sysctl.d/k8s.conf
+cat <<EOF >> /etc/sysctl.d/k8s.conf
 fs.inotify.max_user_instances=8192
 fs.inotify.max_user_watches=1048576
 fs.inotify.max_queued_events=32768
